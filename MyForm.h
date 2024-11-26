f502331797ac04a2618a84da1bd176503b8a0272
Данила Галкин <dryntil228@gmail.com>
2024-11-26 19:34:06 +0300

    yes

--- MyForm.h
+++ MyForm.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <main/main/stack.h>
+#include <msclr/marshal_cppstd.h> 
+#include <string>
 
 namespace CppWinForm1 {
 
@@ -8,6 +11,8 @@ namespace CppWinForm1 {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+
+	TCalc c;
 
 	/// <summary>
 	/// Summary for MyForm
@@ -34,6 +39,10 @@ namespace CppWinForm1 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+	private: System::Windows::Forms::RichTextBox^ richTextBox1;
+	private: System::Windows::Forms::Button^ button1;
 
 	private:
 		/// <summary>
@@ -48,12 +57,96 @@ namespace CppWinForm1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(481, 187);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(0, 13);
+			this->label1->TabIndex = 0;
+			// 
+			// richTextBox1
+			// 
+			this->richTextBox1->Location = System::Drawing::Point(115, 184);
+			this->richTextBox1->Name = L"richTextBox1";
+			this->richTextBox1->Size = System::Drawing::Size(268, 96);
+			this->richTextBox1->TabIndex = 1;
+			this->richTextBox1->Text = L"";
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"PT Utah Condensed", 26.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->Location = System::Drawing::Point(115, 287);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(268, 75);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"Посчитать";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(773, 497);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->richTextBox1);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+
+		
+
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		try {
+			msclr::interop::marshal_context context;
+			std::string infix = context.marshal_as<std::string>(richTextBox1->Text);
+
+
+			c.setInfix(infix);
+
+			double res = c.Calc();
+
+
+
+			System::String^ managedString = System::Convert::ToString(res);
+
+			
+			label1->Text = managedString;
+		}
+		catch (int ex) {
+			Form^ messageBox = gcnew Form();
+			messageBox->Text = L"Ошибочка"; // Заголовок окна
+			messageBox->Size = System::Drawing::Size(300, 100);
+			messageBox->Location = System::Drawing::Point(50, 200);// Размер окна
+
+			// Создаем метку с текстом
+			Label^ label = gcnew Label();
+			label->Text = L"Неверная запись или деление на ноль!";
+			label->Location = System::Drawing::Point(20, 20);
+			label->AutoSize = true;
+
+			// Добавляем метку в окно
+			messageBox->Controls->Add(label);
+
+			// Показываем окно
+			messageBox->ShowDialog();
+		}
+		
+	}
 	};
 }
